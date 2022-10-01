6fa5b2aabb887c4a436457569ea055309d526efc
koudayao27 <113324030+koudayao27@users.noreply.github.com>
2022-10-01 17:11:36 +0800

    Create psle.py

---
+++ psle.py
@@ -0,0 +1,15 @@
+marks = int(input())
+if marks > 90:
+  print("A*")
+elif marks > 74:
+  print("A")
+elif marks > 59:
+  print("B")
+elif marks > 49:
+  print("C")
+elif marks > 34:
+  print("D")
+elif marks > 19:
+  print("E")
+else:
+  print("U")
